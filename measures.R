70eadb9ee8366013d0c5886a1513b76b0cb66cc7
Roger Day <notsofeo@gmail.com>
2016-07-20 23:17:51 -0400

    Added "disease-free survival".

--- measures.R
+++ measures.R
@@ -4,7 +4,8 @@ measureNames = c("[^[:alpha:]]ROC", ###  this works!
                  "hazard ratio", "risk ratio", "risk difference",
                   "odds ratio", 
                  "NNT|number needed to treat", 
-                 "accuracy", "expected utility")
+                 "accuracy", "expected utility",
+                 "disease-free survival")
 measures_in_abstracts = 
   sapply(measureNames, grep, x=all_abstract_text, ignore.case=TRUE)
 ### article 4 has PPV and NPV in the abstract. But the outcome was receiving chemo!
@@ -12,7 +13,7 @@ fronts[[4]]
 all_title_strings[4]
 pmc197$pmc[4]
 pmc197$doi[4]
+pmc197$abstract[4]
+
 #http://www.ncbi.nlm.nih.gov/pmc/?term=10.1007%2Fs10549-016-3833-9%5BDOI%5D
 #http://www.ncbi.nlm.nih.gov/pmc/?term=10.1007/s10549-016-3833-9
-pmc_show = function(n)
-  system(paste0('open "http://www.ncbi.nlm.nih.gov/pmc/?term=', pmc197$doi[n], '"') )
